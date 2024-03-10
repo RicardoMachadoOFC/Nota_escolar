67862a8a25c9094aa615d71eba0a248606249e29
Juvris <cado.machado@hotmail.com>
2024-03-10 15:35:11 -0300

    first commit

---
+++ test.py
@@ -0,0 +1,16 @@
+nota1 = 7
+nota2 = 8
+nota3 = 2
+
+notafinal = (nota1 + nota2 + nota3) / 3
+print(notafinal)
+
+if notafinal >=7:
+   print("voce passou")
+
+else:
+    print("Voce reprovouu")
+
+
+
+     
